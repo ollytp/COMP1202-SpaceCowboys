0ce4efdd593a7e290704fb721b67a75ba32bba84
ollytp <92581230+ollytp@users.noreply.github.com>
2021-10-17 22:58:36 +0100

    Update Challenge1.py

--- Challenge1.py
+++ Challenge1.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 import urllib.request
 import pandas as pd
 
+a = False
 
 
-imp = input("Please input the email credentials \n")
-link = "https://www.ecs.soton.ac.uk/people/" + imp
-source = urllib.request.urlopen(link).read()
-soup = BeautifulSoup(source,'lxml')
-name = str(soup.title)
-namelist = name.split("|")
-print("This is " + (namelist[0])[7:-1] + "! Working at the School of" + namelist[1] + 'at the Un')
+while (a = False):
+  imp = input("Please input the email credentials \n")
+  link = "https://www.ecs.soton.ac.uk/people/" + imp
+  source = urllib.request.urlopen(link).read()
+  soup = BeautifulSoup(source,'lxml')
+  name = str(soup.title)
+  namelist = name.split("|")
+  print("This is " + (namelist[0])[7:-1] + "! Working at the School of" + namelist[1] + 'at the Un')
