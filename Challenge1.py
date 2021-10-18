ff98fd075411d7c49f2d42a6669a6d28d66f7e78
ollytp <92581230+ollytp@users.noreply.github.com>
2021-10-18 09:14:11 +0100

    Update Challenge1.py

--- Challenge1.py
+++ Challenge1.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import urllib.request
-import pandas as pd
+#import pandas as pd
 
-a = False
+a = False #boolean variable 
 
 
-while (a = False):
-  imp = input("Please input the email credentials \n")
-  link = "https://www.ecs.soton.ac.uk/people/" + imp
-  source = urllib.request.urlopen(link).read()
-  soup = BeautifulSoup(source,'lxml')
-  name = str(soup.title)
-  namelist = name.split("|")
-  print("This is " + (namelist[0])[7:-1] + "! Working at the School of" + namelist[1] + 'at the Un')
+while (a = False): #While loop to ensure that the process infinitely loops
+  imp = input("Please input the email credentials \n") #Take the input of the email id from the user
+  link = "https://www.ecs.soton.ac.uk/people/" + imp #Format the weblink 
+  source = urllib.request.urlopen(link).read() #Access the website via urllib
+  soup = BeautifulSoup(source,'lxml') #Extract the sources from the ecs website  
+  name = str(soup.title) #Extract the title of the webpage, the only bit of infomation we need.
+  namelist = name.split("|") #convert the string "name" into a list to seperate the data for output
+  print("This is " + (namelist[0])[7:-1] + "! Working at the School of" + namelist[1] + 'at the University') #Outputs the infomation provided from the webscarper, assuming access to the website is correct.
+#If the input is invalid then the program will just simpily crash :)
